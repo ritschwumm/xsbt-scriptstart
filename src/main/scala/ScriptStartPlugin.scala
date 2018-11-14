a098f3249101064f7401bfa295493aa3b4e8d9d3
Herr Ritschwumm <ritschwumm@djini.de>
2018-11-14 22:32:55 +0100

    udpate sbt and dependencies, deblank

--- src/main/scala/ScriptStartPlugin.scala
+++ src/main/scala/ScriptStartPlugin.scala
@@ -23,15 +23,15 @@ object Import {
 		// arguments given to the main class
 		prefixArguments:Seq[String]			= Seq.empty
 	)
-	
+
 	//------------------------------------------------------------------------------
-	
+
 	val scriptstart				= taskKey[File]("complete build, returns the created directory")
 	val scriptstartAppDir		= settingKey[File]("where to put starts scripts, jar files and extra files")
-	
+
 	val scriptstartZip			= taskKey[File]("complete build, returns the created application zip file")
 	val scriptstartAppZip		= settingKey[File]("where to put the application zip file")
-	
+
 	val scriptstartPackageName	= settingKey[String]("name of the package built")
 	val scriptstartConfigs		= taskKey[Seq[ScriptConfig]]("one or more startscripts to be generated")
 	val scriptstartExtras		= taskKey[Traversable[PathMapping]]("additional resources as a task to allow inclusion of packaged wars etc.")
@@ -42,17 +42,17 @@ object Import {
 object ScriptStartPlugin extends AutoPlugin {
 	//------------------------------------------------------------------------------
 	//## constants
-	
+
 	private val libName	= "lib"
-	
+
 	//------------------------------------------------------------------------------
 	//## exports
-	
+
 	lazy val autoImport	= Import
 	import autoImport._
-	
+
 	override val requires:Plugins		= ClasspathPlugin && plugins.JvmPlugin
-	
+
 	override val trigger:PluginTrigger	= noTrigger
 
 	override lazy val projectSettings:Seq[Def.Setting[_]]	=
@@ -66,7 +66,7 @@ object ScriptStartPlugin extends AutoPlugin {
 							appDir		= scriptstartAppDir.value
 						),
 				scriptstartAppDir		:= scriptstartBuildDir.value / scriptstartPackageName.value,
-				
+
 				scriptstartZip	:=
 						zipTask(
 							streams		= Keys.streams.value,
@@ -75,57 +75,57 @@ object ScriptStartPlugin extends AutoPlugin {
 							appZip		= scriptstartAppZip.value
 						),
 				scriptstartAppZip		:= scriptstartBuildDir.value / (scriptstartPackageName.value + ".zip"),
-				
+
 				scriptstartPackageName	:= Keys.name.value + "-" + Keys.version.value,
 				scriptstartConfigs		:= Seq.empty,
 				scriptstartExtras		:= Seq.empty,
-				
+
 				scriptstartBuildDir		:= Keys.crossTarget.value / "scriptstart"
 			)
-	
+
 	//------------------------------------------------------------------------------
 	//## tasks
-	
+
 	private def buildTask(
-		streams:TaskStreams,	
+		streams:TaskStreams,
 		assets:Seq[ClasspathAsset],
 		configs:Traversable[ScriptConfig],
 		extras:Traversable[(File,String)],
 		appDir:File
 	):File = {
 		streams.log info s"building scriptstart app in ${appDir}"
-		
+
 		val assetDir	= appDir / libName
 		streams.log info s"copying assets to ${assetDir}"
 		assetDir.mkdirs()
 		val assetsToCopy	= assets map { _.flatPathMapping } map (xu.pathMapping anchorTo assetDir)
 		val assetsCopied	= IO copy assetsToCopy
-		
+
 		streams.log info s"copying extras to ${appDir}"
 		val extrasToCopy	= extras map (xu.pathMapping anchorTo appDir)
 		val extrasCopied	= IO copy extrasToCopy
-		
+
 		streams.log info s"creating scripts in ${appDir}"
 		val scripts	=
 				configs flatMap { config =>
 					val assetNames	= assets map { _.name }
-					
+
 					val scriptData	=
 							ScriptData(
 								vmOptions			= config.vmOptions,
 								systemProperties	= config.systemProperties,
-								classPath			= assetNames,	
+								classPath			= assetNames,
 								mainClass			= config.mainClass,
 								prefixArguments		= config.prefixArguments
 							)
-					
+
 					def writeScript(suffix:String, mkScript:ScriptData=>String):File	= {
 						val content	= mkScript(scriptData)
 						var	target	= appDir / (config.scriptName + suffix)
 						IO write (target, content)
 						target
 					}
-					
+
 					val scripts	= Seq(
 						writeScript("",		unixStartScript),
 						writeScript(".bat",	windowsStartScript)
@@ -133,18 +133,18 @@ object ScriptStartPlugin extends AutoPlugin {
 					scripts foreach { _ setExecutable (true, false) }
 					scripts
 				}
-		
+
 		streams.log info "cleaning up"
 		val allFiles	= (xu.find files appDir).toSet
 		val obsolete	= allFiles -- scripts -- assetsCopied -- extrasCopied
 		IO delete obsolete
-		
+
 		appDir
 	}
-	
+
 	/** build app zip */
 	private def zipTask(
-		streams:TaskStreams,	
+		streams:TaskStreams,
 		appDir:File,
 		prefix:String,
 		appZip:File
@@ -156,10 +156,10 @@ object ScriptStartPlugin extends AutoPlugin {
 		)
 		appZip
 	}
-	
+
 	//------------------------------------------------------------------------------
 	//## script generation
-	
+
 	private case class ScriptData(
 		vmOptions:Seq[String],
 		systemProperties:Map[String,String],
@@ -167,7 +167,7 @@ object ScriptStartPlugin extends AutoPlugin {
 		mainClass:String,
 		prefixArguments:Seq[String]
 	)
-	
+
 	// export LC_CTYPE="en_US.UTF-8"
 	private def unixStartScript(data:ScriptData):String	= {
 		val baseFinder	=
@@ -191,7 +191,7 @@ object ScriptStartPlugin extends AutoPlugin {
 				|		BASE="$(dirname "$0")"
 				|	fi
 				"""
-		
+
 		val vmOptions			= data.vmOptions map xu.script.unixHardQuote mkString " "
 		val systemProperties	= xu.script systemProperties data.systemProperties map xu.script.unixHardQuote mkString " "
 		val baseProperty		= xu.script unixSoftQuote (xu.script systemProperty ("scriptstart.base", "$BASE"))
@@ -199,19 +199,19 @@ object ScriptStartPlugin extends AutoPlugin {
 		val mainClass			= xu.script unixHardQuote data.mainClass
 		val prefixArguments		= data.prefixArguments map xu.script.unixHardQuote mkString " "
 		val passArguments		= xu.script unixSoftQuote "$@"
-		
+
 		val fullScript	=
 				s"""
 				|	#!/bin/bash
-				|	
+				|
 				${baseFinder}
-				|	
+				|
 				|	exec java ${vmOptions} ${systemProperties} ${baseProperty} -cp ${classPath} ${mainClass} ${prefixArguments} ${passArguments}
 				"""
-				
+
 		xu.text stripped fullScript
 	}
-		
+
 	private def windowsStartScript(data:ScriptData):String	= {
 		val vmOptions			= data.vmOptions map xu.script.windowsQuote mkString " "
 		val systemProperties	= xu.script systemProperties data.systemProperties	map xu.script.windowsQuote mkString " "
@@ -220,13 +220,13 @@ object ScriptStartPlugin extends AutoPlugin {
 		val mainClass			= xu.script windowsQuote (data.mainClass)
 		val prefixArguments		= data.prefixArguments map xu.script.windowsQuote mkString " "
 		val passArguments		= "%*"
-		
+
 		val fullScript	=
 				s"""
 				|	cd /d %~dp0%
 				|	java ${vmOptions} ${systemProperties} ${baseProperty} -cp ${classPath} ${mainClass} ${prefixArguments} ${passArguments}
 				"""
-			
+
 		xu.script windowsLF (xu.text stripped fullScript)
 	}
 }
